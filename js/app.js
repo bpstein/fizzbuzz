9a37437d48548b711532cae30cb2358545fc7779
Ben Stein <bumstain@gmail.com>
2014-07-30 00:31:56 +0100

    Conditionals log correct output in console

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
 
 	var i = 0; 
+	var fizz = i % 3;
+	var buzz = i % 5;
+	var fizzBuzz = i % 15;
+
 	var loop = function(){
     	while (i <= 100){ 
     		var item = '<li>' + i + '</li>';            
@@ -13,3 +17,27 @@ $(document).ready(function(){
 	loop();
 
 });
+
+
+
+if (fizz = 0) {
+    
+    console.log("Fizz")
+
+    }
+
+else if (buzz = 0) {
+    
+    console.log("Buzz")
+    
+    }
+
+else if (fizzBuzz = 0) {
+    
+    console.log("FizzBuzz")
+    
+    }
+
+else {
+	console.log("i")
+}
