3ffcc9f345782043edad5ac7f5c6e7107a05ae55
Ben Stein <bumstain@gmail.com>
2014-07-29 23:33:25 +0100

    Loop function to count 1 through 100

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
 
+	var i = 0; 
+	var loop = function(){
+    	while (i <= 100){               
+        	console.log(i); 
+        	i++;                     
+    	}
+	};
+
+	loop();
+
 });
